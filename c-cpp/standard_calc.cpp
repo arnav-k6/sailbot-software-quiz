0e13e9181568c859a8a04323e08078f5067a469f
Arnav Kapadia <arnavkapadia@gmail.com>
2026-01-11 15:11:32 -0800

    Implemented both functions and testcases

--- c-cpp/standard_calc.cpp
+++ c-cpp/standard_calc.cpp
@@ -1,6 +1,6 @@
 #include "stdbool.h"
 #include "standard_calc.h"
-
+#include <stdio.h>
 /**
  * @brief Bounds the provided angle between [-180, 180) degrees.
  *
@@ -12,8 +12,28 @@
  *
  * @return float: The bounded angle in degrees.
  */
-float bound_to_180(float angle) {
-    return 0;
+float bound_to_180(float angle)
+{
+    if (angle >= -180 && angle < 180)
+    {
+        return angle;
+    }
+    /* if angle is already in the specified range, simply return the angle*/
+    if (angle >= 0)
+    {
+        while (angle >= 180)
+        {
+            angle -= 360;
+        }
+    }
+    else
+    {
+        while (angle < -180)
+        {
+            angle += 360;
+        }
+    }
+    return angle;
 }
 
 /**
@@ -22,12 +42,36 @@ float bound_to_180(float angle) {
  *  e.g.)
  *      is_angle_between(0, 45, 90) = true
  *      is_angle_between(45, 90, 270) = false
- * 
+ *
  * @param first_angle:  The first bounding angle in degrees.
  * @param middle_angle: The angle in question in degrees.
  * @param second_angle: The second bounding angle in degrees.
  * @return bool: TRUE when `middle_angle` is not in the reflex angle of `first_angle` and `second_angle`, FALSE otherwise
  */
-bool is_angle_between(float first_angle, float middle_angle, float second_angle) {
-    return true;
+bool is_angle_between(float first_angle, float middle_angle, float second_angle)
+{
+    float first = bound_to_180(first_angle); // bound all angles to a value between -180 and 180 using helper function
+    float middle = bound_to_180(middle_angle);
+    float second = bound_to_180(second_angle);
+
+    middle = bound_to_180(middle - first); // offset middle and second by first, making first angle = 0 for simplicity, and for clear second and first value comparision
+    second = bound_to_180(second - first);
+
+    if (second >= 0)
+    { // if second is ahead (greater) than first which is implicitly 0
+        return (second >= middle && middle >= 0);
+        // angle is only between first and second if the distance from first to middle is less than or equal to first and second
+        // AND the middle angle is at or ahead of first (0), if not then it's out of the acute range
+    }
+    else
+    { // if second is less than first (0)
+        return (second <= middle && middle <= 0);
+        // angle is only between first and second if the distance from first to middle is less than or equal to first and second
+        // AND the middle angle is at or behind first (0), if not then it's out of the acute range
+    }
+}
+
+int main(){
+    bool result = is_angle_between(45,90,270);
+    printf("The result is: %d",result);
 }
--- c-cpp/test_standard_calc.cpp
+++ c-cpp/test_standard_calc.cpp
@@ -4,12 +4,23 @@
 
 /**************** Tests for bound_to_180() *******************/
 void test_bound_basic1(CuTest *tc) {
-    CuAssertDblEquals(tc, 0, bound_to_180(0), 0.0001);
+    CuAssertDblEquals(tc, 0.0, bound_to_180(0.0), 0.0001);
+    CuAssertDblEquals(tc, 90.0, bound_to_180(90.0), 0.0001);
+    CuAssertDblEquals(tc, -180.0, bound_to_180(180.0), 0.0001);
+    CuAssertDblEquals(tc, -179.0, bound_to_180(181.0), 0.0001);
+    CuAssertDblEquals(tc, -90.0, bound_to_180(270.0), 0.0001);
+    CuAssertDblEquals(tc, 0.0, bound_to_180(360.0), 0.0001);
 }
 
 /**************** Tests for is_angle_between() *******************/
 void test_between_basic1(CuTest *tc) {
     CuAssertTrue(tc, is_angle_between(0, 1, 2));
+    CuAssertTrue(tc, !is_angle_between(0, 3, 2));
+    CuAssertTrue(tc, is_angle_between(0,45,90));
+    CuAssertTrue(tc, !is_angle_between(45,90,270));
+    CuAssertTrue(tc, is_angle_between(170, -175, -160));
+    CuAssertTrue(tc, !is_angle_between(170, 160, -160));
+    CuAssertTrue(tc, is_angle_between(30, 60, 60));
 }
 
 int main(int argc, char const *argv[]) {
